80e4cfa71c220f0cf62adf05eaae308697f8b15c
LX8369 <LXALT69@outlook.com>
2022-05-12 14:33:08 +1000

    uwu

--- menu.js
+++ menu.js
@@ -18,6 +18,8 @@ document.getElementById("logIn").style.left = (window.innerWidth/2 - 8) + "px"
 document.getElementById("signUp").style.top = (window.innerHeight/2 + 60) + "px"
 document.getElementById("signUp").style.left = (window.innerWidth/2 - 208) + "px"
 
+const socket = io("https://idle-knight.herokuapp.com")
+
 addEventListener("keydown", (e) => {
     if(e.key === "Enter") {
 
@@ -29,8 +31,16 @@ function logIn() {
     let pass = document.getElementById("logPass").value
     console.info("Log In Here. User: " + user + " Pass: " + pass)
 }
-function signIn() {
+function signUp() {
     let user = document.getElementById("signUser").value
     let pass = document.getElementById("signPass").value
+    if ((pass != undefined) && (user != undefined)) {
+        console.log("Yeah")
+        socket.emit("sendData", {type: "signup", username: user, password: pass})
+    }
     console.info("Sign In Here. User: " + user + " Pass: " + pass)
 }
+
+socket.on('usernameTaken', async() => {
+    alert("username taken please try a diff one")
+    })
