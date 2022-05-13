f4c209ee355b5013ce4c0678860d3ea6b9ea715f
LX8369 <LXALT69@outlook.com>
2022-05-13 13:27:45 +1000

    lol

--- js/signup.js
+++ js/signup.js
@@ -1,1 +1,36 @@
+document.getElementById("signUser").style.left = (window.innerWidth/2 - 180) + "px"
+document.getElementById("signPass").style.left = (window.innerWidth/2 - 180) + "px"
 
+
+document.getElementById("signUser").style.top = (window.innerHeight/2 - 40) + "px"
+document.getElementById("signPass").style.top = (window.innerHeight/2 + 20) + "px"
+
+document.getElementById("signUpHeading").style.top = (window.innerHeight/2 - 120) + "px"
+document.getElementById("signUpHeading").style.left = (window.innerWidth/2 - 160) + "px"
+
+document.getElementById("signUp").style.top = (window.innerHeight/2 + 60) + "px"
+document.getElementById("signUp").style.left = (window.innerWidth/2 - 208) + "px"
+
+const socket = io("https://idle-knight.herokuapp.com")
+
+addEventListener("keydown", (e) => {
+    if(e.key === "Enter") {
+
+    } else if (e.key == "Space") {
+        e.preventDefault();
+    }
+})
+
+function signUp() {
+    let user = document.getElementById("signUser").value
+    let pass = document.getElementById("signPass").value
+    if ((pass != undefined || " ") && (user != undefined || " ")) {
+        console.log("Yeah")
+        socket.emit("sendData", {type: "signup", username: user, password: pass})
+    }
+    console.info("Sign In Here. User: " + user + " Pass: " + pass)
+}
+
+socket.on('usernameTaken', async() => {
+    alert("username taken please try a diff one")
+    })
